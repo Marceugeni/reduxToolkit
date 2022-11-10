94f6441882d4a57a181bd1d0b3c4292b4d3c34c6
Marc Eugeni <marc.gimenez@festo.com>
2022-11-10 16:05:15 +0100

    add store and counterSlice

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './counterSlice'
+
+const Counter = () => {
+
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+
+    return (
+        <div>Counter</div>
+    )
+}
+
+export default Counter
